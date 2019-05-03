7a24103eacb842737a5d918da53179c5a2b3b0a7
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2019-05-03 12:08:25 +0800

    Add files via upload

---
+++ perulangan/for.py
@@ -0,0 +1,16 @@
+#for basic
+
+a = 10
+
+for i in range(a):
+    print("perulangan ke - "+str(i))
+
+print('')
+
+#for dengan data variable
+print('#for dengan data')
+
+barang = ['wireless', 'access point', 'wajan bolic']
+
+for i in barang:
+    print(i)
