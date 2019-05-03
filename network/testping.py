bc7f448d23a4749ddc6e61ee2b5aa9e662d66fed
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2019-05-03 12:10:18 +0800

    Add files via upload

---
+++ network/testping.py
@@ -0,0 +1,16 @@
+#test ping
+
+import os
+import socket
+
+akses = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#buat protokol tcp
+
+ipadd = input("masukkan ip address = ")
+
+net = os.system('ping ' + ipadd)
+
+if net == 0:
+    print('berhasil menghubungkan server master')
+
+else:
+    print('gagal menghubungkan server master')
