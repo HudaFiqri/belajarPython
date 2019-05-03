88de0a713a8afb1fbabe538b3cd073f9ef7eae2e
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2019-05-03 12:22:36 +0800

    Update speak.py

--- speak/speak.py
+++ speak/speak.py
@@ -3,11 +3,13 @@ import speech_recognition as sr
 import os
 import wolframalpha
 
+#mesin untuk bicara
 engine = pyttsx3.init('sapi5')
 client = wolframalpha.Client('your_App_ID')
 voices = engine.getProperty('voices')
 engine.setProperty('voices', voices[len(voices)-1].id)
 
+#fungsi untuk bicara
 def speak(audio):
     print('computer = '+audio)
     engine.say(audio)
