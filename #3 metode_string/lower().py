80e15925f61b311b988ee554e8a78c10de401743
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2019-05-03 21:59:08 +0800

    Create lower().py

---
+++ #3 metode_string/lower().py
@@ -0,0 +1,22 @@
+#lower() digunakan untuk membuat tulisan menjadi huruf kecil
+
+a = 'saya suka python'
+b = 'Saya Suka Ngoding'
+c = 'SAYA SUKA LINUX'
+
+
+#tidak dengan huruf kapital
+print('#tidak dengan huruf kapital')
+print(a.lower())
+
+print('')
+
+#dengan huruf kapital di setiap kata
+print('#dengan huruf kapital di setiap kata')
+print(b.lower())
+
+print('')
+
+#dengan huruf kapital di semua kata
+print('#dengan huruf kapital di semua kata')
+print(c.lower())
