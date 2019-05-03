4890680b037245a82426be0e036b4e4cca09fddb
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2019-05-03 21:14:51 +0800

    Update basic.py

--- output_input/basic.py
+++ output_input/basic.py
@@ -1,10 +1,5 @@
 #output dan input di python
 
-#di python penulisan string cukup mudah
-#tidak seperti di bahasa pemprograman yang lain
-#di python hanya perlu 1 baris penulisan string
-#dan itu pun sudah bisa menghasilkan output
-
 #inputan di python
 print('hello world')
 
