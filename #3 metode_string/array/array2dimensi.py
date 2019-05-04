94881cf89330f87965b99d97928f2d7950a6bb46
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2019-05-04 21:32:34 +0800

    git commit

---
+++ #3 metode_string/array/array2dimensi.py
@@ -0,0 +1,14 @@
+#array 2 dimensi
+
+a = [
+        ['linux', 'open source'],
+        ['windows', 'licence'],
+        ['mac', 'licence']
+    ]
+
+#untuk mengakses array 2 dimensi caranya adalah baris, kolom
+#untuk linux, windows, mac itu adalah kolom
+#untuk linux, open source itu adalah kolom
+
+print(a[0][0])#akan menghasilkan linux
+print(a[1][1])#akan menghasilkan licence
