9e617dda9ce57517a368678c7634a78afe6e3f82
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2019-05-04 22:39:24 +0800

    Add files via upload

---
+++ #3 Metode_String/Array/remove().py
@@ -0,0 +1,10 @@
+#remove() bisa digunakan untuk menghapus array
+
+#array variable
+#data array
+
+a = ['pisang', 'durian', 'oli', 'langsat']
+
+#saya ingin menghapus data oli dari a
+
+a.remove('oli')
