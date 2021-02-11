33666afca0826a3d3d028d627fa5816402b7f4d9
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2021-02-11 21:30:27 +0800

    Menambahkan dan memperbaharui code

---
+++ Python_Lanjutan/Waktu/Show Current Time.py
@@ -0,0 +1,11 @@
+###import semua module yang akan digunakan
+from datetime import *
+
+###panggil module now untuk memanggil fungsi wakti
+waktu = datetime.now()
+
+###menampilkan output dari [tahun:bulan:tanggal]
+print(f'{waktu.strftime("%A")}, {waktu.day}-{waktu.month}-{waktu.year}')
+
+###menampilkan output dari [jam:menit:detik]
+print(f'{waktu.hour}:{waktu.minute}:{waktu.second}')
