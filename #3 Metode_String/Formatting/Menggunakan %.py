8cf0441a0fcfdf498ec58fcdc0e50da16f5ebab8
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2021-02-12 10:26:08 +0800

    menambahkan metode string yang lain

---
+++ #3 Metode_String/Formatting/Menggunakan %.py
@@ -0,0 +1,22 @@
+#variable
+text1 = 'linux'
+angka1int = 100
+angka2flo = 10.5
+
+'''
+NOTE*
+%s - digunakan untuk String
+%d - digunakan untuk Integer
+%f - digunakan untuk Float
+
+sumber referensi: https://www.learnpython.org/en/String_Formatting
+'''
+
+#dengan menggunakan % bisa memanggil kata yang berada pada variable yang akan dipanggil
+print('saya menggunakan OS %s'%text1)
+
+#menampilkan output angka integer
+print('angka integer %d'%angka1int)
+
+#menampilkan output angka float
+print('angka float %f'%angka2flo)
---
+++ #3 Metode_String/Formatting/Menggunakan Format().py
@@ -0,0 +1,15 @@
+'''
+dengan menggunakan format, memungkinkan melakukan manipulasi string.
+
+sumber referensi: https://www.w3schools.com/python/ref_string_format.asp
+'''
+
+#masukkan variablenya lalu tambahkan kurawal untuk membuat variablenya
+#lalu panggil dengan format()
+print('aku suka {barang}'.format(barang='pisang'))
+
+#memanggil variable dengan array
+print('aku suka {0} dan aku suka menngunakan OS {1} dan aku menggunakan {2}'.format('bawang', 'linux', 'python'))
+
+#memanggil variabl dengan urutan
+print('linux adalah {} sedangkan windows adalah {}'.format('penguin', 'jendela'))
