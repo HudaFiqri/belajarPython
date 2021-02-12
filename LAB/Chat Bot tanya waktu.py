ab7dbef2a2af70d455cc662cd357bdc189037420
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2021-02-12 19:16:38 +0800

    Perbaikan Penulisan

--- LAB/Chat Bot tanya waktu.py
+++ LAB/Chat Bot tanya waktu.py
@@ -6,20 +6,16 @@ while True:
     if (data == 'jam?' or data == 'jam'):
 
         if (datetime.now().hour < 12):
-            print(
-                'sekarang jam {jam}:{menit} pagi'.format(jam=datetime.now().hour, menit=datetime.now().minute))
+            print('sekarang jam {jam}:{menit} pagi'.format(jam=datetime.now().hour, menit=datetime.now().minute))
 
         elif (datetime.now().hour < 15):
-            print(
-                'sekarang jam {jam}:{menit} siang'.format(jam=datetime.now().hour, menit=datetime.now().minute))
+            print('sekarang jam {jam}:{menit} siang'.format(jam=datetime.now().hour, menit=datetime.now().minute))
 
         elif (datetime.now().hour < 18):
-            print(
-                'sekarang jam {jam}:{menit} sore'.format(jam=datetime.now().hour, menit=datetime.now().minute))
+            print('sekarang jam {jam}:{menit} sore'.format(jam=datetime.now().hour, menit=datetime.now().minute))
 
         elif (datetime.now().hour < 24):
-            print(
-                'sekarang jam {jam}:{menit} malam'.format(jam=datetime.now().hour, menit=datetime.now().minute))
+            print('sekarang jam {jam}:{menit} malam'.format(jam=datetime.now().hour, menit=datetime.now().minute))
 
     elif (data == 'exit'):
         break
