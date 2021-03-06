ff7d321a205c87470f52e1997c7f059ae12ff1bc
kuhakuu04 <mhudafiqri06@gmail.com>
2021-03-06 20:55:08 +0800

    Menambahkan OOP

---
+++ Python_Lanjutan/OOP/Private, Protected, Public Variable/main.py
@@ -0,0 +1,42 @@
+class Variable:
+
+    def __init__(self):
+        '''
+        Public Variable
+
+        Merupakan Variable yang dapat diakses semua orang dan dapat dipanggil dengan mudah seperti biasanya
+        '''
+        self.Var = 'Ini Public Variable'
+
+        '''
+        Protected Variable
+
+        Merupakan variable yang dapat diakses semua orang dan mirip seperti public variable yang membedakannya adalah pada underscore _
+        '''
+        self._Var = 'Ini Protected Variable'
+
+        '''
+        Private Variable
+
+        Merupakan Variable yang tidak dapat diakses. Variable jenis ini dapat diakses melalui panggilan difunction atau method
+        '''
+        self.__Var = 'Ini Private Variable'
+
+    def Pri_Var(self):
+        self.__Var = 'Ini Private Variable Yang Terpanggil'
+        return self.__Var
+
+
+Variable_Class = Variable()
+
+# Contoh untuk pemanggilan untuk public variable
+print(Variable_Class.Var)
+
+# Contoh untuk pemanggilan untuk protected variable
+print(Variable_Class._Var)
+
+# Dan ini contoh untuk private variable
+# karena namanya private jadi tidak bisa dipanggil
+# print(Variable_Class.__Var)
+# untuk memanggil private variable yang dapat terpanggil bisa dilakukan apabila private variablenya berada pada function atau method
+print(Variable_Class.Pri_Var())
